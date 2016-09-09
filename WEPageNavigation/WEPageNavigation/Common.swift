f0ae0862f2350ee5753399a3c3ee99eee7dda103
wangsw <845234177@qq.com>
2016-09-09 18:26:52 +0800

    no message

---
+++ WEPageNavigation/WEPageNavigation/Common.swift
@@ -0,0 +1,28 @@
+//
+//  Common.swift
+//  WEPageNavigation
+//
+//  Created by 姚凤 on 16/9/9.
+//  Copyright © 2016年 姚凤. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct NavigationParam {
+    var ONAV_BAR_BOTTOMLINE_HEIGHT = 2.0
+    var ODOT_COORDINATE = 0.0
+    var ONAVIGATION_BAR_HEIGHT = 44
+    var ONAV_TAB_BAR_HEIGHT = 40
+    var OSTATUS_BAR_HEIGHT = 20
+}
+
+struct DefaultSetting {
+//    func colorWithRGBA(r: Float, g: Float, b:Float,a:Float )->(UIColor) {
+//        return UIColor.init(colorLiteralRed: r, green: g, blue: b, alpha: a)
+//    }
+    
+    var NavTabbarColor = UIColor.init(colorLiteralRed: 240, green: 230, blue: 230, alpha: 1)
+    var DefaultRedColor = UIColor.init(colorLiteralRed: 226, green: 58, blue: 58, alpha: 1)
+    
+}
---
+++ WEPageNavigation/WEPageNavigation/WENaviTabBar.swift
@@ -0,0 +1,21 @@
+//
+//  WENaviTabBar.swift
+//  WEPageNavigation
+//
+//  Created by 姚凤 on 16/9/9.
+//  Copyright © 2016年 姚凤. All rights reserved.
+//
+
+import UIKit
+
+class WENaviTabBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
